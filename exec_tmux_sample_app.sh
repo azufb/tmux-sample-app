08676799b771083556bb0568a7c47aa8a1a8565d
azufb <curiousazunas@gmail.com>
2023-08-14 03:04:12 +0900

    シェル修正

--- exec_tmux_sample_app.sh
+++ exec_tmux_sample_app.sh
@@ -8,13 +8,13 @@ tmux selectp -U
 
 # バックエンドを起動する
 # send-keysでキー入力。-tでペイン指定。
-tmux send-keys -t top 'cd backend' C-m
-tmux send-keys -t top 'npm start' C-m
+tmux send-keys 'cd backend' C-m
+tmux send-keys 'npm start' C-m
 
 # 下のペインを使う
 tmux selectp -D
 
 # フロントエンドを起動する
 # send-keysでキー入力。-tでペイン指定。
-tmux send-keys -t bottom 'cd frontend' C-m
-tmux send-keys -t bottom 'npm start' C-m
+tmux send-keys 'cd frontend' C-m
+tmux send-keys 'npm start' C-m

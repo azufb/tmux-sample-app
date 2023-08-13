7904fe6bf0bb57cfd12099be84ab2394c76ed7d6
azufb <curiousazunas@gmail.com>
2023-08-14 01:48:32 +0900

    パス修正

--- frontend/src/ts/getData.ts
+++ frontend/src/ts/getData.ts
@@ -2,7 +2,7 @@ import axiosInstance from '../axios/axiosInstance';
 import { AxiosResponse } from 'axios';
 
 const getData = async (): Promise<any> => {
-  const result: AxiosResponse = await axiosInstance.get('/api/');
+  const result: AxiosResponse = await axiosInstance.get('/');
   const resultData = result.data;
 
   return resultData;
